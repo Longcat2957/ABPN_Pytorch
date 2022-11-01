529873188fbea2d5a023b345d5273f7c42eab47d
Longcat2957 <schrodingers030@gmail.com>
2022-11-01 15:47:17 +0900

    12

--- q_model_test.py
+++ q_model_test.py
@@ -3,13 +3,13 @@ import torchvision
 from libs.data_utils import *
 from libs.model import *
 from libs.qt import *
-torch.backends.quantized.engine = 'qnnpack'
-WEIGHT_PATH = "./weights/qat_180.pth"
+torch.backends.quantized.engine = 'fbgemm'
+WEIGHT_PATH = "./weights/qat_fbgemm_final.pth"
 q_model = get_q_model(WEIGHT_PATH)
 #saveModel(q_model, "edgeSR_int8_qnnpack.pth")
 net = torch.jit.script(q_model)
-# torch.jit.save(net, "edgeSR_int8_qnnpack_jit.pth")
-net2 = torch.jit.load("edgeSR_int8_qnnpack_jit.pth")
-a = torch.randn(size=(1, 3, 30, 30))
-b = net2(a)
-print(b.shape)
+torch.jit.save(net, "edgeSR_int8_fbgemm_jit.pth")
+# net2 = torch.jit.load("edgeSR_int8_qnnpack_jit.pth")
+# a = torch.randn(size=(1, 3, 30, 30))
+# b = net2(a)
+# print(b.shape)
--- raspberry_demo.py
+++ raspberry_demo.py
@@ -6,51 +6,8 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms as T
 from libs.model import edgeSR
-from libs.qt import qat_wrapper, qat_q_convert
+from libs.qt import get_q_model, qat_wrapper, qat_q_convert
 
+torch.backends.quantized.engine = "qnnpack"
 QAT_WEIGHT_PATH = "./weights/qat_180.pth"
-
-torch.backends.quantized.engine = 'qnnpack'
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
-cap.set(cv2.CAP_PROP_FPS, 16)
-
-preprocess = T.Compose([
-    T.ToTensor()
-])
-
-net = edgeSR()
-net = qat_wrapper(net, "fbgemm")
-try:
-    net.load_state_dict(torch.load(QAT_WEIGHT_PATH))
-except:
-    raise ValueError(QAT_WEIGHT_PATH)
-
-net = qat_q_convert(net)
-net = torch.jit.script(net)
-
-started = time.time()
-last_logged = time.time()
-frame_count = 0
-
-with torch.no_grad():
-    while True:
-        ret, image = cap.read()
-        if not ret:
-            raise RuntimeError("failed to read frame")
-        
-        image = image[:, :, [2, 1, 0]]
-        permuted = image
-        input_tensor = preprocess(image)
-        input_batch = input_tensor.unsqueeze(0)
-        
-        out = net(input_batch)
-        
-        frame_count += 1
-        now = time.time()
-        # Count FPS per 1 seconds
-        if now - last_logged > 1.0:
-            print(f"{frame_count / (now-last_logged)} fps")
-            last_logged = now
-            frame_count = 0
+q_model = get_q_model(QAT_WEIGHT_PATH)
