727812720358105368518a44d02e2f35a7bbf0eb
Longcat2957 <schrodingers030@gmail.com>
2022-11-01 14:51:45 +0900

    1

--- q_model_test.py
+++ q_model_test.py
@@ -3,11 +3,13 @@ import torchvision
 from libs.data_utils import *
 from libs.model import *
 from libs.qt import *
-
+torch.backends.quantized.engine = 'qnnpack'
 WEIGHT_PATH = "./weights/qat_180.pth"
 q_model = get_q_model(WEIGHT_PATH)
 #saveModel(q_model, "edgeSR_int8_qnnpack.pth")
 net = torch.jit.script(q_model)
 # torch.jit.save(net, "edgeSR_int8_qnnpack_jit.pth")
 net2 = torch.jit.load("edgeSR_int8_qnnpack_jit.pth")
-print(net2)
+a = torch.randn(size=(1, 3, 30, 30))
+b = net2(a)
+print(b.shape)
